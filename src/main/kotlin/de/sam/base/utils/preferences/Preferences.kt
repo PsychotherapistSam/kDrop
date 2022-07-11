20629da5cadd017a94c16d53010bb69b08c26b7d
Sam <37251808+PsychotherapistSam@users.noreply.github.com>
2022-07-11 15:36:02 +0200

    add option to toggle file previews

--- src/main/kotlin/de/sam/base/utils/preferences/Preferences.kt
+++ src/main/kotlin/de/sam/base/utils/preferences/Preferences.kt
@@ -4,6 +4,7 @@ class Preferences {
     companion object {
         val preferencesList = listOf(
             Triple("dark-mode", Boolean, "Dark Mode"),
+            Triple("file-previews", Boolean, "Show File Previews"),
         )
     }
 }
