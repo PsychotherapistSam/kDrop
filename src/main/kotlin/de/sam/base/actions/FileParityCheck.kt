ea63d62a2877fddea32ddc9626f533aeab18bd2d
Sam <37251808+PsychotherapistSam@users.noreply.github.com>
2022-09-28 14:26:01 +0200

    feat: add file parity check on startup

---
+++ src/main/kotlin/de/sam/base/actions/FileParityCheck.kt
@@ -0,0 +1,31 @@
+package de.sam.base.actions
+
+import de.sam.base.controllers.isValidUUID
+import de.sam.base.database.FileDAO
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
+import java.util.*
+
+class FileParityCheck {
+    fun checkIfLocalFilesExistInDatabase() {
+        transaction {
+            val localFilesList = File("./upload")
+                .walk()
+                .toList()
+                .map { it.name }
+                .filter { it.isValidUUID() }
+                .map { UUID.fromString(it) }
+
+            val existingFiles = FileDAO.forIds(localFilesList).map { it.id.value }
+
+            val filesNotInDB = localFilesList.filter { !existingFiles.contains(it) }
+            // get all files that do not exist in the database
+//            val filesThatDoNotExistInDatabase = localFilesList.filter { FileDAO.findById(it) == null }
+
+            filesNotInDB.forEach {
+                println("File $it does not exist in the database, deleting it locally...")
+                File("./upload/$it").delete()
+            }
+        }
+    }
+}
