5486fdf07737cc92afec8c96ae2c7a6695c90821
Sam <37251808+PsychotherapistSam@users.noreply.github.com>
2024-02-18 17:26:51 +0100

    feat: add created date to api key

--- src/main/kotlin/de/sam/base/database/ApiKey.kt
+++ src/main/kotlin/de/sam/base/database/ApiKey.kt
@@ -2,6 +2,7 @@ package de.sam.base.database
 
 import org.jdbi.v3.core.mapper.RowMapper
 import org.jdbi.v3.core.statement.StatementContext
+import org.joda.time.DateTime
 import java.io.Serializable
 import java.sql.ResultSet
 import java.util.*
@@ -9,7 +10,8 @@ import java.util.*
 data class ApiKeyDTO(
     var id: UUID,
     var apiKey: String,
-    var user: UUID
+    var user: UUID,
+    val createdAt: DateTime,
 ) : Serializable
 
 class ApiKeyDTOMapper : RowMapper<ApiKeyDTO> {
@@ -17,7 +19,8 @@ class ApiKeyDTOMapper : RowMapper<ApiKeyDTO> {
         return ApiKeyDTO(
             id = UUID.fromString(rs.getString("id")),
             apiKey = rs.getString("api_key"),
-            user = UUID.fromString(rs.getString("user_id"))
+            user = UUID.fromString(rs.getString("user_id")),
+            createdAt = DateTime(rs.getTimestamp("created_at"))
         )
     }
 }
--- src/main/resources/db/migration/V4__add_apikey_table.sql
+++ src/main/resources/db/migration/V4__add_apikey_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE t_api_keys
 (
-    id      uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    api_key VARCHAR(255) NOT NULL,
-    user_id uuid         NOT NULL,
+    id         uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    api_key    VARCHAR(255) NOT NULL,
+    user_id    uuid         NOT NULL,
+    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES t_users (id)
 );
